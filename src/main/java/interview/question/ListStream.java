e04f4a30eb1890cb716466a601ea9db4697db080
balajraj <brajagop@gmail.com>
2018-06-18 11:17:52 +0200

    Added the last but one element of a list

--- src/main/java/interview/question/ListStream.java
+++ src/main/java/interview/question/ListStream.java
@@ -1,6 +1,8 @@
 package interview.question;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class ListStream<T> {
 
@@ -8,6 +10,18 @@ public class ListStream<T> {
     public T last() {
         T ret = list.stream().reduce((first,second) -> second).orElse(null);
         return ret;
+    }
+
+    public T secondLast() throws NoSuchElementException {
+        long count = list.stream().count();
+        T second = null;
+        if ( count >= 2 ) {
+            second = list.stream().skip(count -2 ).findFirst().orElseThrow(() -> new NoSuchElementException());
+        }
+        else {
+           throw new NoSuchElementException();
+        }
+        return second;
     }
 
     public void setList(List<T> list) {
--- src/test/java/TestStream.java
+++ src/test/java/TestStream.java
@@ -10,11 +10,12 @@ public class TestStream {
 
 
     private ListStream<String> list;
+    private ListStream<Integer> listNum;
 
     @Before
     public void setup() {
         list = new ListStream<String>();
-
+        listNum = new ListStream<Integer>();
     }
 
     @Test
@@ -22,4 +23,25 @@ public class TestStream {
         list.setList(Arrays.asList("a", "b", "c", "d") );
         assertThat(list.last(), is(equalTo("d")));
     }
+
+    @Test
+    public void shouldFindSecondLastElementFromAList() throws Exception {
+        listNum.setList(Arrays.asList(1, 2, 11, 4, 5, 8, 10, 6));
+        assertThat(listNum.secondLast(), is(equalTo(10)));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldThrowExceptionWhenListEmpty() throws Exception {
+        listNum.setList(Collections.emptyList());
+        listNum.secondLast();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldThrowExceptionWhenListHasSingleElement() throws Exception {
+        listNum.setList(Arrays.asList(1));
+        listNum.secondLast();
+    }
+
+
+
 }
